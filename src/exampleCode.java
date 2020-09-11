038701b57b2271b9b8d2fdeed90d2b3c93387758
JakeEgarhos <jakee1354@gmail.com>
2020-09-11 16:47:02 -0700

    feature 1 added. Simple JUnit test implemented

--- src/exampleCode.java
+++ src/exampleCode.java
@@ -18,7 +18,19 @@ public class exampleCode {
 		System.out.println("Would you like to add or subtract the two numbers? (0 for add, 1 for subtract)");
 		int addOrSub = input.nextInt();
 
+		//Feature 1 - Add the two numbers and print the result
 		
+		
+		if (addOrSub == 0) {
+			System.out.println("The sum is " + add(num1, num2)); 
+			}
+		}
+
+			
+	public static int add (int num1, int num2) {
+		int result = num1 + num2;
+		return result; 
 	}
 }
+
 
---
+++ src/exampleCodeTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class exampleCodeTest {
+
+	@Test
+	void testAdd() {
+		assertEquals(792, exampleCode.add(427, 365));
+	}
+
+}
